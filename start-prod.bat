ce6afe880f9b117ee7bfeb5ad218315e5086022a
Your Name <you@example.com>
2026-09-13 00:01:12 +0300

    Clean repo and add new products

--- start-prod.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\vadim\Desktop\Смартфоны\megapixel-store
-node node_modules\next\dist\bin\next start -p 3000 -H 0.0.0.0
--- start-tunnel.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\vadim\Desktop\Смартфоны\megapixel-store
-npx localtunnel --port 3000
